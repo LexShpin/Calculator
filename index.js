e0e4712b49d4545401d15a362f73dd8782673202
lexshpin <lexshpin@gmail.com>
2022-04-21 21:16:21 -0600

    Added basic layout, writing out the logic

---
+++ index.js
@@ -0,0 +1,50 @@
+const userInput = document.querySelector('.user-input');
+const resultField = document.querySelector('.result-field');
+
+const clearBtn = document.querySelector('.clear-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+
+const calculatorBtns = document.querySelectorAll('.calculator-btn');
+
+let firstOperand;
+let secondOperand;
+let operation;
+let result;
+
+clearBtn.addEventListener('click', () => {
+    userInput.textContent = '';
+    firstOperand = 0;
+    secondOperand = 0;
+    result = 0;
+});
+
+deleteBtn.addEventListener('click', () => {
+    userInput.textContent;
+});
+
+calculatorBtns.forEach(btn => btn.addEventListener('click', (e) => {
+    // console.log(e.target.textContent);
+    userInput.textContent += e.target.textContent.trim();
+}));
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
